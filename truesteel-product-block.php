37098e0277bb3b62dcc62c541f2b33a9d7fac40d
Nourfawzy-x <122273948+Nourfawzy-x@users.noreply.github.com>
2024-06-24 18:06:06 +0300

    Update truesteel-product-block.php

--- truesteel-product-block.php
+++ truesteel-product-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:      truesteel-product-block
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       A versatile WordPress block for showcasing products with customizable title, description, image,and  call-to-action button.
+
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       truesteel-product-block
